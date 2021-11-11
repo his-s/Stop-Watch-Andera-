bc0fc765696b1c4af2c6cf1cecf477f51532c752
his-s <tanton509@gamil.com>
2021-11-11 16:51:21 +0200

    play with matrix widget and matrix 4

--- lib/ui/stopwatch_renderer.dart
+++ lib/ui/stopwatch_renderer.dart
@@ -1,19 +1,27 @@
+import 'dart:math';
+
 import 'package:animation_app/ui/elapsed_time_text.dart';
 import 'package:flutter/material.dart';
 
 class StopWatchRenderer extends StatelessWidget {
-  const StopWatchRenderer({Key? key,required this.elapsed}) : super(key: key);
+  const StopWatchRenderer({Key? key, required this.elapsed}) : super(key: key);
   final Duration elapsed;
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          color: Colors.indigo,
+        Transform(
+          transform: Matrix4.identity()
+            ..translate(50.0, 0.0, 0.0)
+            ..rotateZ(pi / 4),
+          alignment: Alignment.center,
+          child: Container(
+            color: Colors.indigo,
+          ),
         ),
-        ElapsedTimeText(
-          elapsed: elapsed,
-        ),
+        // ElapsedTimeText(
+        //   elapsed: elapsed,
+        // ),
       ],
     );
   }
