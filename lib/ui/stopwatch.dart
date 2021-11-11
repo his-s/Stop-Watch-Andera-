13657ad29738b714465127a487a8d29d3671d050
his-s <tanton509@gamil.com>
2021-11-11 15:35:10 +0200

    replace time periodic with ticker

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,32 +1,32 @@
 import 'dart:async';
 import 'package:animation_app/ui/elapsed_time_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class Stopwatch extends StatefulWidget {
   @override
   _StopwatchState createState() => _StopwatchState();
 }
 
-class _StopwatchState extends State<Stopwatch> {
+class _StopwatchState extends State<Stopwatch>
+    with SingleTickerProviderStateMixin {
   Duration _eplapsed = Duration.zero;
-  late DateTime _initialTime;
-  late final Timer _timer;
+
+  late final Ticker _ticker;
   @override
   void initState() {
     super.initState();
-    _initialTime = DateTime.now();
-    _timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
-      final now = DateTime.now();
+    _ticker = createTicker((elapsed) {
       setState(() {
-       
-        _eplapsed = now.difference(_initialTime);
+        _eplapsed = elapsed;
       });
     });
+    _ticker.start();
   }
 
   @override
   void dispose() {
-    _timer.cancel();
+    _ticker.stop();
     super.dispose();
   }
 
