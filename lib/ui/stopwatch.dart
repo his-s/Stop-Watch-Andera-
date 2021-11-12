cf9f37e127affdd0af406c24dfc45a3d11116d82
his-s <tanton509@gamil.com>
2021-11-12 15:14:19 +0200

    complete clock application

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:animation_app/ui/elapsed_time_text.dart';
+import 'package:animation_app/ui/reset_button.dart';
+import 'package:animation_app/ui/start_stop_button.dart';
 import 'package:animation_app/ui/stopwatch_renderer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -11,18 +13,19 @@ class Stopwatch extends StatefulWidget {
 
 class _StopwatchState extends State<Stopwatch>
     with SingleTickerProviderStateMixin {
-  Duration _eplapsed = Duration.zero;
-
+  Duration _prevElapsed = Duration.zero;
+  Duration _currentlyElapsed = Duration.zero;
+  Duration get _eplapsed => _prevElapsed + _currentlyElapsed;
+  bool _isRuning = false;
   late final Ticker _ticker;
   @override
   void initState() {
     super.initState();
     _ticker = createTicker((elapsed) {
       setState(() {
-        _eplapsed = elapsed;
+        _currentlyElapsed = elapsed;
       });
     });
-    _ticker.start();
   }
 
   @override
@@ -31,11 +34,58 @@ class _StopwatchState extends State<Stopwatch>
     super.dispose();
   }
 
+  void _toggleStart() {
+    setState(() {
+      _isRuning = !_isRuning;
+    });
+    if (_isRuning) {
+      _ticker.start();
+    } else {
+      _ticker.stop();
+      _prevElapsed += _currentlyElapsed;
+      _currentlyElapsed = Duration.zero;
+    }
+  }
+
+  void _reset() {
+    _ticker.stop();
+    setState(() {
+      _isRuning = false;
+      _prevElapsed = Duration.zero;
+      _currentlyElapsed = Duration.zero;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constrains) {
       final radius = constrains.maxWidth / 2;
-      return StopWatchRenderer(elapsed: _eplapsed, radius: radius);
+      return Stack(
+        children: [
+          StopWatchRenderer(elapsed: _eplapsed, radius: radius),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: ResetButton(
+                onPressed: _reset,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: StartStopButton(
+                onPressed: _toggleStart,
+                isRunning: _isRuning,
+              ),
+            ),
+          ),
+        ],
+      );
     });
   }
 }
